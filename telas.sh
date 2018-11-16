f4b27359536ede8ca16e43ed875e9ba02c50070f
Allan Neri Fernandes da Silva <allan.fernandes@dataprev.gov.br>
2018-11-16 09:38:54 -0300

    Comportamento da função 'valida entrada'

--- telas.sh
+++ telas.sh
@@ -15,13 +15,22 @@ function tl_listarUsuarios {
 
 function valida_entrada {
 	#verifica se foi recebido valores por parametro
+	if [ $# -eq 0  ]
+	then
+		echo "Nenhuma entrada foi dada para ser valiadada"
+
 	#busca apenas as ocorrencias de '|'  e conta quantas existem
 	_nome=`echo $1 | grep -o '|' | wc -l `
 
 	if [ $_nome -eq 1 ]
 	then
 		echo $1 >> contatos
+		return 1
+	else
+		return 0
+		# indica que existe '|' na entrada
 	fi
 	#caso não esteja valido: retornar falso para que a função
 	#que o chamou solicite novamente a inserção dos dados
+	
 }
