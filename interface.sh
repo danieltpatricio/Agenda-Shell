7b4821788f85712e70c6ce099891f697ce375e93
Daniel Teixeira Patricio <daniel@Daniels-MacBook-Air.local>
2018-12-06 18:17:29 -0300

    teste

---
+++ interface.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+################################
+#
+#	Autor: Allan Neri (Daniel refatoração)
+#	Descrição: Funções uteis para trabalhar com telas 
+#	que o usuário irá interagir com o sistema.
+#
+#
+################################
+
+# importar agendapi
+file=Agenda.csv
+
+function tela_addUsuario {
+
+	zenity --forms --title="Adicionar Contato:" \
+	--text="Preencha os dados do seu contato." \
+	--separator="," \
+	--add-entry="Nome:" \
+	--add-entry="Telefone:" >> $file
+	case $? in
+		0)
+			echo "Contato Adicionado."
+			;;
+		1)
+			echo "Contato cancelado."
+			;;
+		-1)
+			echo "Erro ao cadastrar contato."
+			;;
+	esac
+	tela_principal
+
+}
+
+function tela_listarUsuarios {
+	
+	#aqui os valores da lista serão obtidos de um arquivo
+	valoresLista=""
+	if [[ -z $1 ]]
+	then
+		tituloPagina="Contatos Cadastrados"
+	else
+		tituloPagina=$1
+	fi
+	zenity --list --text="$tituloPagina" --column=Nome --column=Telefone $valoresLista
+
+}
+
+function valida_entrada {
+	#verifica se foi recebido valores por parametro
+	if [ $# -eq 0  ]
+		then
+			echo "Nenhuma entrada foi dada para ser valiadada"
+	fi
+	#busca apenas as ocorrencias de '|'  e conta quantas existem
+	_nome=`echo $1 | grep -o '|' | wc -l `
+
+	#caso não esteja valido: retornar falso para que a função
+	#que o chamou solicite novamente a inserção dos dados
+	if [[ $_nome -eq 1 ]]; then
+		echo $1 >> Agenda.csv
+		return 1
+	else
+		return 0
+		# indica que existe '|' na entrada
+	fi
+	
+}
+
+function tela_delContato {
+	
+	tela_listarUsuarios "Selecione Usuário para Excluir"
+
+	if [[ $? -eq 1 ]] # clicou no botão cancelar
+	then
+		tela_principal
+	fi
+
+	usr_selecionado=$?
+
+}
+
+function tela_principal {
+	
+	#utilizar estas variaveis para verificar a entrada selecionada
+	lstContato="Contatos Cadastrados"
+	addContato="Adicionar Contato"
+	delContato="Excluir Contato"
+	sair="Sair"
+	
+	
+	retorno=`zenity --info --text=Lista --ok-label="$lstContato" --extra-button="$addContato" --extra-button="$delContato" --extra-button="$sair"`;
+	if [[ $? == 0 ]] 
+	then
+		tela_listarUsuarios
+	fi
+	case $retorno in
+		$addContato )
+			tela_receberNovoUsuario
+		;;
+		$delContato )
+			tela_delContato
+		;;
+		$sair )
+			exit 1
+		;;
+	esac
+}
+tela_addUsuario
